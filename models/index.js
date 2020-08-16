e60f895e397df7b3b52b0a296bdedaffb12c9940
J Mulder <jlmulder2014@gmail.com>
2020-08-15 17:51:07 -0700

    update database models and associations

--- models/index.js
+++ models/index.js
@@ -5,12 +5,31 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+  onDelete: "cascade",
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  //??
+  as: 'product_tags',
+  foreignKey: 'product_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  //?? is this right?  criteria is different
+  as: 'product_tags',
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
